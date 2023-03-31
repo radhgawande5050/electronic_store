fccf648c908e25e86e0e0474797121ed904dc3b7
radhgawande5050 <radhgawande5050@gmail.com>
2023-03-31 17:16:50 +0530

    done by Radha

---
+++ src/main/java/com/bikkadit/config/AppConstant.java
@@ -0,0 +1,15 @@
+package com.bikkadit.config;
+
+public class AppConstant {
+    public static final String USER_NOT_FOUND="User not found with this id !!";
+
+    public static final String USER_DELETE="user is deleted successfull !!";
+
+    public static final String YES="yes";
+
+    public static final String NO="no";
+
+    public static final String CATEGORY_NOT_FOUND="category not found";
+
+
+}
