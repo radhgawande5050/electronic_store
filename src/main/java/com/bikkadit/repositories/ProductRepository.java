8e02c023bae71211da645dd9e8fbfb907ab607ac
radhgawande5050 <radhgawande5050@gmail.com>
2023-04-04 23:41:44 +0530

    changes done in ProductRepository

--- src/main/java/com/bikkadit/repositories/ProductRepository.java
+++ src/main/java/com/bikkadit/repositories/ProductRepository.java
@@ -3,5 +3,11 @@ package com.bikkadit.repositories;
 import com.bikkadit.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByTitleContaining(String subtitle);
+
+    List<Product>findByLiveTrue();
 }
