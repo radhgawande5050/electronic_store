70759f52ec1cdf6c419819438ecb777c7bccbb26
radhgawande5050 <radhgawande5050@gmail.com>
2023-04-04 23:35:53 +0530

    changes done

--- src/main/java/com/bikkadit/dtos/CategoryDto.java
+++ src/main/java/com/bikkadit/dtos/CategoryDto.java
@@ -8,18 +8,20 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDto extends BaseEntityDto {
     private Long categoryId;
-    @NotBlank
-    @Min(value=4, message = "title must be of minimum 4 characters !!")
+   @Size(min=4, message = "title must be of minimum 4 characters !!")
     private String title;
 
-    @NotBlank(message = "Description is Required !!")
-    private String Description;
+    @Size(min=30,max=80,message = "Description is Required !!")
+    private String description;
 
     private String coverImage;
 }
