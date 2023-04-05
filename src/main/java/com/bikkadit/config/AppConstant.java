c45bc9f516e09c3b74b054db53e0d7bde82f5782
radhgawande5050 <radhgawande5050@gmail.com>
2023-04-05 09:28:20 +0530

    changes done in AppConstant

--- src/main/java/com/bikkadit/config/AppConstant.java
+++ src/main/java/com/bikkadit/config/AppConstant.java
@@ -1,15 +1,25 @@
 package com.bikkadit.config;
 
+import com.bikkadit.services.impl.CategoryServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppConstant {
     public static final String USER_NOT_FOUND="User not found with this id !!";
 
-    public static final String USER_DELETE="user is deleted successfull !!";
+    public static final String USER_DELETE="user is deleted successfully !!";
 
     public static final String YES="yes";
 
     public static final String NO="no";
 
-    public static final String CATEGORY_NOT_FOUND="category not found";
+    public static final String CATEGORY_NOT_FOUND="category not found with this id";
+
+    public static final String CATEGORY_DELETE="category is deleted successfully !!";
+
+    public static final String PRODUCT_NOT_FOUND="product not found with this id";
+
+    public static final String PRODUCT_DELETE="product is deleted successfully !!";
 
 
 }
